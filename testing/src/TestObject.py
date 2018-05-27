946c7e70e78977de08cf5bd1239d0dfa9972e061
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-05-27 19:43:00 +0200

    Improve mock examples

--- testing/src/TestObject.py
+++ testing/src/TestObject.py
@@ -7,3 +7,7 @@ class TestObject():
         print("Hello world !")
         raise Exception("Method is not mocked !")
 
+    def sayHelloTo(self, name):
+        print("Hello " + name + " !")
+        raise Exception("Method is not mocked !")
+
--- testing/tests/MockTest.py
+++ testing/tests/MockTest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from src import TestObject
 
 
@@ -10,3 +10,12 @@ class MockTest(unittest.TestCase):
         testInstance = TestObject()
         testInstance.sayHello = MagicMock(return_value=3)
         self.assertEqual(testInstance.sayHello(), 3)
+
+    @patch('src.TestObject.sayHello', return_value=9)
+    def test_mockMethod2(self, sayHello):
+        self.assertEqual(sayHello(), 9)
+
+    @patch('src.TestObject.sayHelloTo', return_value=10)
+    def test_mockMethod2(self, sayHelloTo):
+        self.assertEqual(sayHelloTo("Bob"), 10)
+        sayHelloTo.assert_called_with("Bob")
